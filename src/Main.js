1d1d137873d5b2a80d61156d7dcf26cea77cd4f8
Leon Hayden <leon.hayden@gmx.com>
2018-06-13 19:11:24 -0500

    add chat and sidebar comments

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import Sidebar from './Sidebar'
+
+import Chat from './Chat' 
+
+class Main extends Component {
+    render ()  {
+        return (
+         <div className= 'Main'>
+            <Sidebar />
+            <Chat />
+            </div>   
+        )
+    }
+}
+
+export default Main
