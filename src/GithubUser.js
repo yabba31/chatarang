80332d457bba2cbef6e61b407e22d0efcef056ed
yabba31 <39818048+yabba31@users.noreply.github.com>
2018-06-20 06:13:36 -0400

    Create GithubUser.js

---
+++ src/GithubUser.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class GithubUser extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: {}
+    }
+
+    this.fetchUserData()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.username !== this.props.match.params.username) {
+      this.fetchUserData()
+    }
+  }
+
+  fetchUserData = () => {
+    const { params } = this.props.match
+    fetch(`https://api.github.com/users/${params.username}`)
+      .then(response => response.json())
+      .then(user => this.setState({ user }))
+  }
+
+  render() {
+    const { user } = this.state
+
+    return (
+      <div className="GithubUser">
+        <img src={user.avatar_url} alt="" />
+        <h2>
+          <a href={user.html_url} target="_blank">
+            {user.name} ({user.login})
+          </a>
+        </h2>
+        <h3>{user.location}</h3>
+      </div>
+    )
+  }
+}
+
+export default GithubUser
